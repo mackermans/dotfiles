effee58978dd9831e626972012252be980c5c1e2
Maarten Ackermans <4571935+mackermans@users.noreply.github.com>
2024-03-07 22:09:13 +0700

    chore: clean up unused x86_64-linux sha

--- overlays/gh-copilot.nix
+++ overlays/gh-copilot.nix
@@ -12,14 +12,12 @@ stdenv.mkDerivation rec {
       {
         "aarch64-darwin" = "https://github.com/github/gh-copilot/releases/download/${version}/darwin-arm64";
         "aarch64-linux" = "https://github.com/github/gh-copilot/releases/download/${version}/linux-arm64";
-        "x86_64-linux" = "https://github.com/github/gh-copilot/releases/download/${version}/linux-amd64";
       }
       .${stdenv.hostPlatform.system};
     hash =
       {
         "aarch64-darwin" = "sha256-F2OA66h/ptkjLZ2oQgkbZlDo31YDZzhk5Pre36TkHvI=";
         "aarch64-linux" = "sha256-4vX9On0upgfjM/IL/UzQj5ioeVnSsd2rUgIz6w4szZM=";
-        "x86_64-linux" = "sha256-AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA=";
       }
       .${stdenv.hostPlatform.system};
   };
