bcbc006baf8d4a186c5c8d44a9f5252c9eb11726
Maarten Ackermans <4571935+mackermans@users.noreply.github.com>
2024-03-11 16:34:24 +0800

    feat: tweak mini-files width, set default dir explorer, disable permanent delete

--- users/batman/.config/nvim/lua/plugins/mini-files.lua
+++ users/batman/.config/nvim/lua/plugins/mini-files.lua
@@ -2,14 +2,22 @@ return {
   "echasnovski/mini.files",
   opts = {
     windows = {
+      -- Maximum number of windows to show side by side
+      max_number = math.huge,
+      -- Whether to show preview of file/directory under cursor
       preview = true,
-      width_focus = 30,
-      width_preview = 30,
+      -- Width of focused window
+      width_focus = 50,
+      -- Width of non-focused window
+      width_nofocus = 15,
+      -- Width of preview window
+      width_preview = 25,
     },
     options = {
+      -- Whether to delete permanently or move into module-specific trash
+      permanent_delete = true,
       -- Whether to use for editing directories
-      -- Disabled by default in LazyVim because neo-tree is used for that
-      use_as_default_explorer = false,
+      use_as_default_explorer = true,
     },
   },
   keys = {
