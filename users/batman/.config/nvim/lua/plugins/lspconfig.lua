1d5ca3411311d9ed8f208d031a4ff234a1560f97
Maarten Ackermans <4571935+mackermans@users.noreply.github.com>
2024-03-19 20:28:58 +0700

    feat: eslint format on save

--- users/batman/.config/nvim/lua/plugins/lspconfig.lua
+++ users/batman/.config/nvim/lua/plugins/lspconfig.lua
@@ -18,6 +18,12 @@ return {
         taplo = {},
 
         eslint = {
+          on_attach = function(client, bufnr)
+            vim.api.nvim_create_autocmd("BufWritePre", {
+              buffer = bufnr,
+              command = "EslintFixAll",
+            })
+          end,
           settings = {
             -- helps eslint find the eslintrc when it's placed in a subfolder instead of the cwd root
             workingDirectories = { mode = "auto" },
