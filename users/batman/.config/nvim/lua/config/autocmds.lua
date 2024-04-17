32af584c02b3cf8e6775eb5e6ef40521edb2badd
Maarten Ackermans <4571935+mackermans@users.noreply.github.com>
2024-04-17 15:44:08 +0700

    fix(nvim): fix issue with telescope insert mode after opening file buffer

--- users/batman/.config/nvim/lua/config/autocmds.lua
+++ users/batman/.config/nvim/lua/config/autocmds.lua
@@ -1,3 +1,13 @@
 -- Autocmds are automatically loaded on the VeryLazy event
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
+
+-- Fixes an issue with Telescope where you're stuck in insert mode after opening a file buffer with Telescope
+-- https://github.com/nvim-telescope/telescope.nvim/issues/2501#issuecomment-1561838990
+vim.api.nvim_create_autocmd("WinLeave", {
+  callback = function()
+    if vim.bo.ft == "TelescopePrompt" and vim.fn.mode() == "i" then
+      vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes("<Esc>", true, false, true), "i", false)
+    end
+  end,
+})
