07743731e27c8cb9e48c7512bd054d020b9642ac
Maarten Ackermans <4571935+mackermans@users.noreply.github.com>
2024-04-28 19:48:44 +0700

    feat(jj): add SSH_AUTH_SOCK for git ssh agent

--- users/batman/programs.nix
+++ users/batman/programs.nix
@@ -238,6 +238,9 @@
           then ''
             # homebrew
             eval "$(/opt/homebrew/bin/brew shellenv)"
+
+            # 1password ssh agent compatibility for git used by jujutsu
+            set -gx SSH_AUTH_SOCK ~/Library/Group\ Containers/2BUA8C4S2C.com.1password/t/agent.sock
           ''
           else ''
           ''
