d41b5a94d60c1d1f3f57e2fed26881121e66f5b7
Maarten Ackermans <4571935+mackermans@users.noreply.github.com>
2024-05-27 13:44:29 +0800

    feat(wezterm): set window blur and RESIZE window decorations

--- users/batman/.config/wezterm/config.lua
+++ users/batman/.config/wezterm/config.lua
@@ -1,11 +1,18 @@
 local wezterm = require("wezterm")
 
-return {
-	color_scheme = "Catppuccin Mocha",
-	enable_tab_bar = false,
-	font = wezterm.font("FiraCode Nerd Font Mono"),
-	font_size = 16.0,
-	line_height = 1.2,
-	-- window_decorations = "RESIZE",
-  xcursor_theme = "Adwaita",
-}
+local config = wezterm.config_builder()
+
+config.color_scheme = "Catppuccin Mocha"
+config.xcursor_theme = "Adwaita"
+
+config.enable_tab_bar = false
+
+config.font = wezterm.font("FiraCode Nerd Font Mono")
+config.font_size = 16.0
+config.line_height = 1.2
+
+config.window_decorations = "RESIZE"
+config.window_background_opacity = 0.75
+config.macos_window_background_blur = 8
+
+return config
