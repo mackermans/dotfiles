e4c21b2a3d04a7ef8b51ffc4773679ed87464c83
Maarten Ackermans <4571935+mackermans@users.noreply.github.com>
2024-05-30 12:53:39 +0800

    fix(nvim): silence no-info lsp messages on hover

--- users/batman/.config/nvim/lua/plugins/noice.lua
+++ users/batman/.config/nvim/lua/plugins/noice.lua
@@ -1,6 +1,13 @@
 return {
   "folke/noice.nvim",
   opts = {
+    lsp = {
+      hover = {
+        -- Set not show a message if hover is not available
+        -- ex: shift+k on Typescript code
+        silent = true,
+      },
+    },
     presets = {
       bottom_search = false,
       command_palette = true,
