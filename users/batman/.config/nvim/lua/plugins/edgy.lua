1920d3201ec4a2d1d2883072e40c7eb74f9a61c7
Maarten Ackermans <4571935+mackermans@users.noreply.github.com>
2024-07-06 00:06:27 +0100

    feat: add copilot-chat

--- users/batman/.config/nvim/lua/plugins/edgy.lua
+++ users/batman/.config/nvim/lua/plugins/edgy.lua
@@ -45,7 +45,10 @@ return {
               return vim.api.nvim_win_get_config(win).relative == ""
             end,
           },
-          { ft = "qf", title = "QuickFix" },
+          {
+            ft = "qf",
+            title = "QuickFix",
+          },
           {
             ft = "help",
             size = { height = 20 },
@@ -54,11 +57,26 @@ return {
               return vim.bo[buf].buftype == "help"
             end,
           },
-          { title = "Spectre", ft = "spectre_panel", size = { height = 0.4 } },
-          { title = "Neotest Output", ft = "neotest-output-panel", size = { height = 15 } },
+          {
+            title = "Spectre",
+            ft = "spectre_panel",
+            size = { height = 0.4 },
+          },
+          {
+            title = "Neotest Output",
+            ft = "neotest-output-panel",
+            size = { height = 15 },
+          },
         },
         left = {
           { title = "Neotest Summary", ft = "neotest-summary" },
+        },
+        right = {
+          {
+            ft = "copilot-chat",
+            title = "Copilot Chat",
+            size = { width = 50 },
+          },
         },
         keys = {
           -- increase width
