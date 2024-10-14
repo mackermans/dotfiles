b205c20b3ed19f264e0734ce7e17b52456454c06
Maarten Ackermans <4571935+mackermans@users.noreply.github.com>
2024-10-14 11:28:14 +0100

    feat(jj): add bookmark set fish completion

---
+++ users/batman/.config/fish/completions/jj.fish
@@ -0,0 +1,6 @@
+function __jj_bookmark_set_complete
+    jj bookmark list -T 'name ++ "\n"' 2>/dev/null
+end
+
+complete -c jj -n "__fish_seen_subcommand_from bookmark; and __fish_seen_subcommand_from set" -a "(__jj_bookmark_set_complete)" -f
+
