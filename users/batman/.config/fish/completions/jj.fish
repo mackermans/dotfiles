c8a7f8986fe3b71c62e8b30a8c5395ecebcacf0b
Maarten Ackermans <4571935+mackermans@users.noreply.github.com>
2024-10-16 10:34:04 +0100

    feat(jj): add completion for jj new

--- users/batman/.config/fish/completions/jj.fish
+++ users/batman/.config/fish/completions/jj.fish
@@ -3,4 +3,5 @@ function __jj_bookmark_set_complete
 end
 
 complete -c jj -n "__fish_seen_subcommand_from bookmark; and __fish_seen_subcommand_from set" -a "(__jj_bookmark_set_complete)" -f
+complete -c jj -n "__fish_seen_subcommand_from new" -a "(__jj_bookmark_set_complete)" -f
 
